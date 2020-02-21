3a86e3df2bf5771138c51651f79e25a16a83d057
vf-rahman-mahmoodi <rahman.mahmoodi@valueflow.com.au>
2020-02-21 12:56:09 +1100

    Using valueflowit2020 instead.

--- src/configs/FirebaseConfig.js
+++ src/configs/FirebaseConfig.js
@@ -1,24 +1,24 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyBFPOwK5BEWVJexeHPkipuGE64VnPBiPJw",
-  authDomain: "reacttodo-a80ce.firebaseapp.com",
-  databaseURL: "https://reacttodo-a80ce.firebaseio.com",
-  projectId: "reacttodo-a80ce",
-  storageBucket: "reacttodo-a80ce.appspot.com",
-  messagingSenderId: "346964367327",
-  appId: "1:346964367327:web:740b167d25b629b56c566a",
-  measurementId: "G-XZ7W3ELCBH"
-};
-
-// VF Config: https://console.firebase.google.com/project/todoapp-45364
-// var firebaseConfig = {
-//   apiKey: "AIzaSyCVpIMuBonpgoLbbFQPLEyvH3xgGCqH41E",
-//   authDomain: "todoapp-45364.firebaseapp.com",
-//   databaseURL: "https://todoapp-45364.firebaseio.com",
-//   projectId: "todoapp-45364",
-//   storageBucket: "todoapp-45364.appspot.com",
-//   messagingSenderId: "768040276126",
-//   appId: "1:768040276126:web:e84a937dbaa5aab1fdab8f",
-//   measurementId: "G-466X9R54DJ"
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBFPOwK5BEWVJexeHPkipuGE64VnPBiPJw",
+//   authDomain: "reacttodo-a80ce.firebaseapp.com",
+//   databaseURL: "https://reacttodo-a80ce.firebaseio.com",
+//   projectId: "reacttodo-a80ce",
+//   storageBucket: "reacttodo-a80ce.appspot.com",
+//   messagingSenderId: "346964367327",
+//   appId: "1:346964367327:web:740b167d25b629b56c566a",
+//   measurementId: "G-XZ7W3ELCBH"
 // };
+
+// VF Config: https://console.firebase.google.com/project/reacttodoapp-36559/database/reacttodoapp-36559/data
+var firebaseConfig = {
+  apiKey: "AIzaSyB-lbVnCpcnm5yQ4AZa1WpH3J4QxLjvVJI",
+  authDomain: "reacttodoapp-36559.firebaseapp.com",
+  databaseURL: "https://reacttodoapp-36559.firebaseio.com",
+  projectId: "reacttodoapp-36559",
+  storageBucket: "reacttodoapp-36559.appspot.com",
+  messagingSenderId: "173239996425",
+  appId: "1:173239996425:web:ba1cfc87a163dca4dc68d8",
+  measurementId: "G-Z1NY51Z3BN"
+};
 
 export default firebaseConfig;
